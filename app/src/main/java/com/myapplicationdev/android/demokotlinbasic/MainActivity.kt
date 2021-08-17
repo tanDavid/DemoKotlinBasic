949a3ba2c3a52c9a87cefcaf2e4272ccba5f5fb1
19002765 <19002765@myrp.edu.sg>
2021-08-17 11:22:15 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/demokotlinbasic/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.myapplicationdev.android.demokotlinbasic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnGetAge.setOnClickListener {
+            val userDOB = etDOB.text.toString()
+            if (userDOB.isNotEmpty()) {
+                val year = Calendar.getInstance().get(Calendar.YEAR)
+                val age = year - userDOB.toInt()
+
+                var output = ""
+                if(age < 18) {
+                    output = "Underage"
+                }else if (age in 18..65){
+                    output = "Young People"
+                }else if (age in 80..99){
+                    output = "Elderly"
+                }else{
+                    output = "Long-lived elderly"
+                }
+
+                tvShowAge.text = "Your age is $age \n $output"
+            }else{
+                Toast.makeText(this, "Please enter your birth year", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+}
