eaca53a94f1a782a016dc134fde6a5b93172fb84
19002765 <19002765@myrp.edu.sg>
2021-08-17 11:25:50 +0800

    completed

--- app/src/main/java/com/myapplicationdev/android/demokotlinbasic/MainActivity.kt
+++ app/src/main/java/com/myapplicationdev/android/demokotlinbasic/MainActivity.kt
@@ -18,14 +18,24 @@ class MainActivity : AppCompatActivity() {
                 val age = year - userDOB.toInt()
 
                 var output = ""
-                if(age < 18) {
-                    output = "Underage"
-                }else if (age in 18..65){
-                    output = "Young People"
-                }else if (age in 80..99){
-                    output = "Elderly"
-                }else{
-                    output = "Long-lived elderly"
+//                if(age < 18) {
+//                    output = "Underage"
+//                }else if (age in 18..65){
+//                    output = "Young People"
+//                }else if (age in 80..99){
+//                    output = "Elderly"
+//                }else{
+//                    output = "Long-lived elderly"
+//                }
+
+                when(age){
+                    in 0..17 -> output = "Underage"
+                    in 18..65 -> output = "Young People"
+                    in 66..79 -> output = "Middle-aged"
+                    in 80..99 -> output = "Elderly"
+                    else -> {
+                        output = "Long-lived elderly"
+                    }
                 }
 
                 tvShowAge.text = "Your age is $age \n $output"
